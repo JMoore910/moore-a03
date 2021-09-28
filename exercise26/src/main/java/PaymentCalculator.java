4c0ad57d92c4852a08da508335869a5371f94e0e
Jeanne Moore <mooresea910@gmail.com>
2021-09-28 18:29:24 -0400

    Exercise 26 pseudocode

--- exercise26/src/main/java/PaymentCalculator.java
+++ exercise26/src/main/java/PaymentCalculator.java
@@ -1,8 +1,21 @@
+import static java.lang.Math.*;
+
 public class PaymentCalculator {
     //create a constructor
+    //Variables should be private for PaymentCalculator's use ONLY
+    //Create getters and setters for user change variables in a rational way
+    //variables:
+    //int numMonths
+    //double dailyRate
+    //double balance
+    //double monthlyPayment
 
     public static int calculateMonthsUntilPaidOff(){
-        /*Pseudocode goes here!*/
+        //method uses class variables
+
+        //numMonths = -(1/30) * log(1 + balance/monthlyPayment * (1 - pow(1 + dailyRate,30)) / log(1 + dailyRate);
+
+
         return 0;
     }
 }
--- exercise26/src/main/java/Solution26.java
+++ exercise26/src/main/java/Solution26.java
@@ -14,6 +14,12 @@
 */
 public class Solution26 {
     public static void main(String[] args) {
-        /*PseudoCode Goes Here!*/
+        //get user input for balance, APR as a percentage, and payPerMonth
+        //divide APR by 100 first to get it as a decimal,
+        //then divide APR by 365 to get the daily rate on the card.
+        //Construct an object of type PaymentCalculator
+        //use the class setters to give all of the input to the object and then make it
+        //run its calculateMonthsUntilPaidOff method
+        //return the result as a part of the output
     }
 }
