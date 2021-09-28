3a16734157c55116cf8e8a113196ee7ec3f24a0f
Jeanne Moore <mooresea910@gmail.com>
2021-09-28 11:20:20 -0400

    Solution24 test created

---
+++ exercise24/src/main/java/Solution24.java
@@ -0,0 +1,13 @@
+/*
+ *   UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *   Copyright 2021 Sean Moore
+ */
+
+//import statements
+import java.util.Scanner;
+
+public class Solution24 {
+    public static void main(String[] args) {
+        //First commit
+    }
+}
---
+++ exercise24/src/test/java/Solution24Test.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution24Test {
+    @Test
+    public void test_etcetcetcCHANGETHISASNEEDED() {
+        Solution24 sol24 = new Solution24();
+        //Point p1 = new Point(0,0);
+        //Point p2 = new Point(1,0);
+
+        //double actual_distance = sol24.getDistance(p1,p2);
+        //double expected_distance = 1;
+        assertEquals(0,0, 0.000001);
+    }
+}
