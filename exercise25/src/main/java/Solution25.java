d19b07b5fb197a56f60ac199f10d5706d08f3bd2
Jeanne Moore <mooresea910@gmail.com>
2021-09-28 15:39:25 -0400

    ex25 completed

--- exercise25/src/main/java/Solution25.java
+++ exercise25/src/main/java/Solution25.java
@@ -12,60 +12,89 @@
     very strong: String contains letters, numbers, and special characters  at least 8 characters
  */
 
-public class Solution25 {
-    //public String passwordValidator(String A){
-    //if string is less than 8 characters
-    //      strength is either very weak, weak, or unknown(less than 8 but mix of chars)
-    //      if A.matches("[0-9]+"){
-    //          password is purely numeric, return 1 (very weak)
-    //      else if A.matches("[a-zA-Z]+")
-    //          password is purely letters , return 2 (weak)
-    //      else
-    //          neither case is met, return 0 (unknown)
-    //else if string is more than 8 characters
-    //      strength is either strong, very strong, or unknown
-    //      int numCount=0, alphaCount=0, specCount=0;
-    //      use .isDigit and .isAlphabetic to find the count of letters, numbers, and special characters
-    //      using if-else statements
-    //      for (int i = 0; i < A.length(); i++){
-    //          if (Character.isDigit(A.charAt(i)){
-    //              numCount++;
-    //          else if (Character.isAlphabetic(A.charAt(i)){
-    //              alphaCount++;
-    //          else
-    //              specCount++;
-    //
-    //      if (alphaCount > 0 && numCount > 0 && specCount == 0)
-    //          password contains at least one number, no special chars, return 3 (strong)
-    //      else if (alphaCount > 0 && numcount > o && specCount > 0)
-    //          password contains at least one of each type of char, return 4 (very strong)
-    //      else
-    //          password contains only one type of char, return 0 (unknown)
+import java.util.Scanner;
 
+public class Solution25 {
+    public static int passwordValidator(String A) {
+        if (A.length() < 8) {
+            //string is less than 8 characters
+            //strength is either very weak, weak, or unknown(less than 8 but mix of chars)
+
+            //password is purely numeric, return 1 (very weak)
+            if (A.matches("[0-9]+"))
+                return 1;
+
+            //password is purely letters , return 2 (weak)
+            else if (A.matches("[a-zA-Z]+"))
+                return 2;
+
+            //neither case is met, return 0 (unknown)
+            else
+                return 0;
+        } else {
+            //more than 8 characters
+            //strength is either strong, very strong, or unknown
+            int numCount=0, alphaCount=0, specCount=0;
+            //use .isDigit and .isAlphabetic to find the count of letters, numbers, and special characters
+            for (int i = 0; i < A.length(); i++) {
+                         if (Character.isDigit(A.charAt(i)))
+                             numCount++;
+                         else if (Character.isAlphabetic(A.charAt(i)))
+                             alphaCount++;
+                         else
+                             specCount++;
+            }
+            //password contains at least one number, letters, no special chars, return 3 (strong)
+            if (alphaCount > 0 && numCount > 0 && specCount == 0)
+                return 3;
+
+            //password contains at least one of each type of char, return 4 (very strong)
+            else if (alphaCount > 0 && numCount > 0 && specCount > 0)
+                return 4;
+
+            //password contains only one type of char, return 0 (unknown)
+            else
+                return 0;
+
+        }
+    }
 
     public static void main(String[] args) {
-        //int strength
-        //String output
-        //prompt for a string to be input and receive input
-        //pass the string into password validator and assign returned statement to an int strength
-        //use a switch function to assign a string a rating based on int strength [0-4]
-        //Switch strength
-        //case 1:
-        //  output is very weak
-        //  break
-        //case 2:
-        //  output is weak
-        //  break
-        //case 3:
-        //  output is strong
-        //  break
-        //case 4:
-        //  output is very strong
-        //  break
-        //default case:
-        //  output is "unknown"
-        //  break
+        int strength;
+        String password;
+        String output;
+        Scanner input = new Scanner(System.in);
 
+        //prompt for a string to be input and receive input
+        System.out.printf("Please enter a password to check strength: ");
+        password = input.nextLine();
+
+        //pass the string into password validator and assign returned statement to an int strength
+        strength = passwordValidator(password);
+
+        //use a switch function to assign a string a rating based on int strength [0-4]
+        switch (strength) {
+            case 1:
+            output = "very weak";
+            break;
+
+            case 2:
+            output = "weak";
+            break;
+
+            case 3:
+            output = "strong";
+            break;
+
+            case 4:
+            output = "very strong";
+            break;
+
+            default:
+            output = "unknown strength";
+            break;
+        }
         //with output concatenate strength to the end of output to describe password in a single output statement
+        System.out.println("The password '" + password + "' is a " + output + " password.");
     }
 }
---
+++ exercise25/src/test/java/Solution25Test.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution25Test {
+    @Test
+    public void test_password_validation() {
+        Solution25 sol25 = new Solution25();
+        int expected_strength;
+        int actual_strength;
+        String password = "123456"; //1
+        assertEquals(1,sol25.passwordValidator(password));
+        password = "abcdef"; //2
+        assertEquals(2,sol25.passwordValidator(password));
+        password = "abcd1234"; //3
+        assertEquals(3,sol25.passwordValidator(password));
+        password = "abc123!@#"; //4
+        assertEquals(4,sol25.passwordValidator(password));
+        password = "********"; //0
+        assertEquals(0,sol25.passwordValidator(password));
+        password = "abcdefgh"; //0
+        assertEquals(0,sol25.passwordValidator(password));
+        password = "12345678"; //0
+        assertEquals(0,sol25.passwordValidator(password));
+        password = "***"; //0
+        assertEquals(0,sol25.passwordValidator(password));
+    }
+}
