dcad9deba9c236c176b30b2ce2c744ed8ac4a1e4
Jeanne Moore <mooresea910@gmail.com>
2021-09-28 13:13:31 -0400

    sol24 pseudocode

--- exercise24/src/main/java/Solution24.java
+++ exercise24/src/main/java/Solution24.java
@@ -6,8 +6,26 @@
 //import statements
 import java.util.Scanner;
 
+/*
+    Create a program that compares two strings to see if they are anagrams
+    Create method: stringCompare(String A, String B){
+    use for-each loops to find out if A is an anagram of B. }
+ */
+
 public class Solution24 {
+    //stringCompare
+
+    //Create a program that compares two strings to see if they are anagrams
+    //Create method: stringCompare(String A, String B){
+    //use for-each loops to find out if A is an anagram of B.
+    //return a boolean flag anagram, //true if anagram, false if not anagram
+
     public static void main(String[] args) {
-        //First commit
+        //prompt for input from user
+        //receive the two strings from User as input
+        //call the stringCompare(str1,str2) within an if statement checking if true is returned
+        //  print "the strings are anagrams"
+        //else
+        //  print "the strings are not anagrams"
     }
 }
