085a130c2f5844e07e12d2af3155faa5e3f1fda4
Jeanne Moore <mooresea910@gmail.com>
2021-09-30 15:28:16 -0400

    Exercise33 pseudocode

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,34 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sean Moore
+ */
+
+/*
+    Create a magic 8-ball program that has stored responses in a list
+    get input from user, then throw it away and choose a random response from the list
+    Create method: magicEightBall(int randNum)
+    receives a randomly generated number from 0-3, then looks at a list for a response, and outputs that response
+ */
+
+public class Solution33 {
+
+    //  Create method: public static void magicEightBall(int randNum)
+    //      List<String> responseList = new ArrayList<String>();
+    //      Add the responses to the list
+    //      responseList.add("Yes");
+    //      responseList.add("No");
+    //      responseList.add("Maybe");
+    //      responseList.add("Ask again later.");
+    //      print out the response at randNum index
+    //      print: responseList(randNum)
+
+    public static void main(String[] args) {
+        //  Prompt for user to input a temp string as a question to a magic 8-ball
+        //  generate a random integer and send it into the magicEightBall method
+        //  Random rand = new Random();
+        //  int randInt = rand.nextInt(4);
+        //  magicEightBall(randInt);
+    }
+}
