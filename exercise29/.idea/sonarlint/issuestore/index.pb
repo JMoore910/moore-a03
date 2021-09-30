a5e4c5edc093ac450c2ae487f6fcd6173cfd048d
Jeanne Moore <mooresea910@gmail.com>
2021-09-30 10:45:57 -0400

    Exercise30 pseudocode

---
+++ exercise29/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+Q
+!src/test/java/Solution29Test.java,8\1\81618d5b8743e7ada4b98f8d1f80b2ddb457d7b8
+V
+&src/main/java/baseline/Solution29.java,9\5\95454940577f71189b8edc339b7b513e0a91fe12
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,33 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sean Moore
+ */
+
+/*
+    Create an application that generates a multiplication table for the numbers 1 through 12 (inclusive)
+    Create a method that uses a nested loop to multiply and output the table.
+ */
+
+public class Solution30 {
+
+    //  Create Method: public void multiplyTable()
+    //      int i, j
+    //      for i from 1 to 12
+    //          for j from 1 to 12
+    //              if i*j < 10
+    //                  print 4spaces + i*j
+    //              else if i*j < 100
+    //                  print 3spaces + i*j
+    //              else
+    //                  print 2spaces + i*j
+    //          print \n
+
+
+    public static void main(String[] args) {
+        //No input is needed in any form.
+        //multiplyTable() is called and is a simple output function
+        //No JUnit tests are required as the only function used is an output function
+    }
+}
