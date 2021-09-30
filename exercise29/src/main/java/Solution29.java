6ad039e8925ee0d5fa6165d51de6e22e32ee349d
Jeanne Moore <mooresea910@gmail.com>
2021-09-30 00:44:39 -0400

    exercise29 pseudocode

--- exercise29/src/main/java/Solution29.java
+++ exercise29/src/main/java/Solution29.java
@@ -3,8 +3,33 @@
  *  Copyright 2021 first_name last_name
  */
 
+/*
+    Create an application that validates user input in ensuring proper input
+    Create a number validating function which does not allow the user to input
+    zero. It also makes sure the user cannot input non-numeric values. The function
+    should use a loop to trap bad input
+ */
+
 public class Solution29 {
+    //Create a function: boolean validateInput (String number)
+    //  if the string is not numeric or 0,
+    //      print: Sorry. That's not a valid input.
+    //      return false
+    //  else, return true
+
+    //Create a function: int calculateReturn (int rate)
+    //  int returnTime = 72/rate
+    //  if 72%rate != 0
+    //      returnTime ++;
+    //  return returnTime;
+
     public static void main(String[] args) {
-        
+        //  initialize boolean valid = false
+        //  While loop: while(!valid)
+        //      Prompt for user to input rate of return
+        //      valid = validInput(rate)
+        //
+        //int returnTime = CalculateReturn(rate);
+        //print "It will take " + returnTime + " to double your initial investment."
     }
 }
