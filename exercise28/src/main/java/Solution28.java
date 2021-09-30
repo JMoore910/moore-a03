b72bd4a2ba530a25c1052f8fb88f610c1dd5e274
Jeanne Moore <mooresea910@gmail.com>
2021-09-29 23:41:23 -0400

    exercise28 pseudocode

--- exercise28/src/main/java/Solution28.java
+++ exercise28/src/main/java/Solution28.java
@@ -3,6 +3,27 @@
  *   Copyright 2021 Sean Moore
  */
 
+/*
+    Create an application that takes 5 integers as input,
+    and uses repetition via a counting loop to add them together
+    and prints the sum as output to the user
+    Create a function: Loop function, returns integer sum
+ */
 
 public class Solution28 {
+    //  Create method: calculateSum()
+
+    //  public int calculateSum()
+    //      int sum = 0;
+    //      int num;
+    //      for (int i=0; i<5; i++)
+    //          Prompt for num as input
+    //          sum += num
+    //      return sum;
+
+
+    public static void main(String[] args) {
+        // int sum = calculateSum()
+        // print: "The total is " + sum + "."
+    }
 }
