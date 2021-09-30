f3e39c0f81d9a479dc7db1d7e4a97fa2707b68e4
Jeanne Moore <mooresea910@gmail.com>
2021-09-30 16:07:51 -0400

    exercise34 pseudocode

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,48 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Sean Moore
+ */
+
+/*
+    Create an application that has a list of five employee names and allows the user
+    to input an employee name to remove. The application then searches the list for the
+    specified name, and removes it.
+    Create Method: public static LinkedList<String> createLinkedList()
+    Create Method: public static LinkedList<String> removeNode(LinkedList<String> nameList, String name)
+    Create Method: public static void printNameList(LinkedList<String> nameList)
+ */
+
+public class Solution34 {
+    //  Create method: public static LinkedList<String> createLinkedList()
+    //      nameList.add("John Smith");
+    //      nameList.add("Jackie Jackson");
+    //      nameList.add("Chris Jones");
+    //      nameList.add("Amanda Cullen");
+    //      nameList.add("Jeremy Goodwin");
+    //      return nameList;
+
+
+    //  Create method: public static LinkedList<String> removeNode(LinkedList<String> nameList, String name)
+    //      nameList.remove(name);
+    //      return nameList;
+
+
+    //  Create method: public static void printNameList(LinkedList<String> nameList)
+    //  print: "There are " + nameList.size + " employees"
+    //  for (String name : nameList)
+    //      printline: name
+
+
+    public static void main(String[] args) {
+        //  LinkedList<String> nameList = new LinkedList<String>();
+        //  nameList = createLinkedList();
+        //  printNameList(nameList);
+        //  prompt user to enter an employee name to remove
+        //  print: "Enter an employee name to remove: "
+        //  receive name as input from user
+        //  removeNode(nameList, name);
+        //  printNameList(nameList);
+    }
+}
