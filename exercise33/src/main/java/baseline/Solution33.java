76e3317dd7be121b675c86aa5511c02c1ab2a747
Jeanne Moore <mooresea910@gmail.com>
2021-09-30 15:41:51 -0400

    exercise33 complete

--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -12,23 +12,33 @@ package baseline;
     receives a randomly generated number from 0-3, then looks at a list for a response, and outputs that response
  */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.List;
+
 public class Solution33 {
 
-    //  Create method: public static void magicEightBall(int randNum)
-    //      List<String> responseList = new ArrayList<String>();
-    //      Add the responses to the list
-    //      responseList.add("Yes");
-    //      responseList.add("No");
-    //      responseList.add("Maybe");
-    //      responseList.add("Ask again later.");
-    //      print out the response at randNum index
-    //      print: responseList(randNum)
+    public static void magicEightBall(int randNum) {
+        List<String> responseList = new ArrayList<String>();
+        //      Add the responses to the list
+        responseList.add("Yes");
+        responseList.add("No");
+        responseList.add("Maybe");
+        responseList.add("Ask again later.");
+        //      print out the response at randNum index
+        System.out.printf(responseList.get(randNum));
+        //Simple output statement method, no test required
+    }
 
     public static void main(String[] args) {
         //  Prompt for user to input a temp string as a question to a magic 8-ball
+        System.out.printf("What's your question? \n> ");
+        Scanner input = new Scanner(System.in);
+        input.nextLine();
         //  generate a random integer and send it into the magicEightBall method
-        //  Random rand = new Random();
-        //  int randInt = rand.nextInt(4);
-        //  magicEightBall(randInt);
+        Random rand = new Random();
+        int randInt = rand.nextInt(4);
+        magicEightBall(randInt);
     }
 }
