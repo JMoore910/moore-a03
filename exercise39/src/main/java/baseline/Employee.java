1ad5808dc1612d39161baec3bc269bcb531b262a
Jeanne Moore <mooresea910@gmail.com>
2021-10-02 20:12:41 -0400

    Exercise39 complete

--- exercise39/src/main/java/baseline/Employee.java
+++ exercise39/src/main/java/baseline/Employee.java
@@ -7,24 +7,45 @@ package baseline;
 
 public class Employee {
 
-    //  private String firstName;
-    //  private String lastName;
-    //  private String occupation;
-    //  private String termination;
-    //  public Employee()
+    private String firstName;
+    private String lastName;
+    private String occupation;
+    private String termination;
 
 
     //  Using overloaded constructors allows for setters to be ignored
     //  constructor if termination is included
-    //  public Employee(String firstName, String lastName, String occupation, String termination)
-    //      this.firstName = firstName;
-    //      this.lastName = lastName
-    //      this.occupation = occupation
-    //      this.termination = termination
+    public Employee(String firstName, String lastName, String occupation, String termination) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.occupation = occupation;
+        this.termination = termination;
+    }
 
     //  constructor if termination is excluded
-    //  public Employee(String firstName, String lastName, String occupation)
-    //      this.firstName = firstName;
-    //      this.lastName = lastName
-    //      this.occupation = occupation
+    public Employee(String firstName, String lastName, String occupation) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.occupation = occupation;
+        this.termination = " ";
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getOccupation(){
+        return occupation;
+    }
+
+    public String getTermination(){
+        return termination;
+    }
+
+
+
 }
--- exercise39/src/main/java/baseline/Solution39.java
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -13,68 +13,78 @@ package baseline;
     data must be stored in a list of maps
     Create Class: Employee
     Create Object: Comparator<HashMap<String, Employee>> compareMaps = new Comparator<>()
-    Create Method: ArrayList<HashMap<String, String>> buildList()
-    Create Method: ArrayList<HashMap<String, String>> listSort(List<HashMap<String, Employee>> list)
-    Create Method: void printList(List<HashMap<String, String>> list)
+    Create Method: ArrayList<HashMap<String, Employee>> buildList()
+    Create Method: ArrayList<HashMap<String, Employee>> sortList(ArrayList<HashMap<String, Employee>> list)
+    Create Method: void printList(ArrayList<HashMap<String, Employee>> list)
  */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution39 {
-    //  public static final String key = "employee";
+    public static final String key = "employee";
 
 
-    //  Create method: public Comparator<HashMap<String, Employee>> compareMaps = new Comparator<>()
-    //      comparator is an object that can be used with Collections to sort a list, providing instructions to .sort()
-    //      public int compare(HashMap<String, Employee> emp1, HashMap<String, Employee> emp2)
-    //          return emp1.get("employee").getLastName().compareTo(emp2.get(key).getLastName());
+    private static Comparator<HashMap<String, Employee>> compareMaps = new Comparator<>() {
+        //      comparator is an object that can be used with Collections to sort a list, providing instructions to .sort()
+        public int compare(HashMap<String, Employee> emp1, HashMap<String, Employee> emp2) {
+            return emp1.get(key).getLastName().compareTo(emp2.get(key).getLastName());
+        }
+    };
+
+    public static ArrayList<HashMap<String, Employee>> buildList() {
+        ArrayList<HashMap<String, Employee>> list = new ArrayList<>();
+        HashMap<String, Employee> employee = new HashMap<>();
+        //  Declare and set a map for each employee on record, then add them to the list one by one
+        employee.put(key, new Employee("John","Johnson","Manager","2016-12-31"));
+        list.add(employee);
+        employee = new HashMap<>();
+        employee.put(key, new Employee("Tou","Xiong","Software Engineer","2016-10-05"));
+        list.add(employee);
+        employee = new HashMap<>();
+        employee.put(key, new Employee("Michaela","Michaelson","District Manager","2015-12-19"));
+        list.add(employee);
+        employee = new HashMap<>();
+        employee.put(key, new Employee("Jake","Jacobson","Programmer"));
+        list.add(employee);
+        employee = new HashMap<>();
+        employee.put(key, new Employee("Jacquelyn","Jackson","DBA"));
+        list.add(employee);
+        employee = new HashMap<>();
+        employee.put(key, new Employee("Sally","Webber","Web Developer","2015-12-18"));
+        list.add(employee);
+
+        //      All in all there are six employees to add to the list
+        return list;
+    }
 
 
-    //  Create method: public static ArrayList<Map<String, Employee>> buildList()
-    //      public static List<HashMap<String, Employee>> list = new ArrayList<>();
-    //      HashMap<String, Employee> employee = new HashMap<>();
-    //      Declare and set a map for each employee on record, then add them to the list one by one
-    //      employee.put(key, new Employee("insert strings for constructor"))
-    //      list.add(employee)
-    //      employee = new HashMap<>();
-    //      employee.put(key, new Employee("insert strings for constructor"))
-    //      list.add(employee)
-    //      employee = new HashMap<>();
-    //      employee.put(key, new Employee("insert strings for constructor"))
-    //      .......
-    //      list.add(employee)
-    //
-    //      All in all there are six employees to add to the list
-    //      return list;
+    public static ArrayList<HashMap<String, Employee>> sortList(ArrayList<HashMap<String, Employee>> list) {
+        Collections.sort(list, compareMaps);
+        return list;
+    }
 
 
-    //  Create method: public static ArrayList<HashMap<String, String>> listSort(List<HashMap<String, Employee>> list)
-    //      return Collections.sort(list, compareMaps);
-
-
-    //  Create method: public static void printList(ArrayList<HashMap<String, String>> list)
-    //      print:"Name                | Position          | Separation Date
-    //      print:"--------------------|-------------------|------------------------
-    //      for (int i = 0; i < list.size(); i ++)
-    //          int stringLength = list.get(i).get(key).getFirstName().length() + list.get(i).get(key).getLastName().length() + 1
-    //          int numSpaces = 20 - stringLength
-    //          print: list.get(i).get(key).getFirstName() + " " + list.get(i).get(key).getLastName()
-    //          for (int j = 0; j < numSpaces; j++)
-    //              print: " "
-    //          stringLength = list.get(i).get(key).getOccupation().length()
-    //          numSpaces = 18 - stringLength
-    //          print: "| " + list.get(i).get(key).getOccupation()
-    //          for (j = 0; j < numSpaces; j++)
-    //              print: " "
-    //          print "| " + list.get(i).get(key).getTermination()
-
+    public static void printList(ArrayList<HashMap<String, Employee>> list) {
+        System.out.println("Name                | Position          | Separation Date");
+        System.out.println("--------------------|-------------------|-----------------");
+        for (int i = 0; i < list.size(); i ++) {
+            int stringLength = list.get(i).get(key).getFirstName().length() + list.get(i).get(key).getLastName().length() + 1;
+            int numSpaces = 20 - stringLength;
+            System.out.printf(list.get(i).get(key).getFirstName() + " " + list.get(i).get(key).getLastName());
+            for (int j = 0; j < numSpaces; j++)
+                System.out.printf(" ");
+            stringLength = list.get(i).get(key).getOccupation().length();
+            numSpaces = 18 - stringLength;
+            System.out.printf("| " + list.get(i).get(key).getOccupation());
+            for (int j = 0; j < numSpaces; j++)
+                System.out.printf(" ");
+            System.out.println("| " + list.get(i).get(key).getTermination());
+        }
+    }
 
     public static void main(String[] args) {
-        //  ArrayList<HashMap<String, Employee>> employeeList = buildList();
-        //  sortList(employeeList);
-        //  printList(employeeList);
+        ArrayList<HashMap<String, Employee>> employeeList = buildList();
+        sortList(employeeList);
+        printList(employeeList);
     }
 }
